11d2365a79082334372d0a88ea8e7b6dba3134ed
Olli Saukko <ollisaukko@Olli-MacBook-Air.local>
2026-01-05 19:34:28 +0200

    avaus heitto logiikka

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@ using namespace std;
 int main ()
 {
 srand (time(0));
-int dice1 = (rand()% 6) + 1;
-int dice2 = (rand()% 6) + 1;
-int dice3 = (rand()% 6) + 1;
-int dice4 = (rand()% 6) + 1;
-int dice5 = (rand()% 6) + 1;
-cout << "You rolled: " << dice1 << " " << dice2 << " " << dice3 << " " << dice4 << " " << dice5 << endl;
+int dice[5];
+bool locked[5] = {false, false, false, false, false};
+
+// avaus heitto 
+for (int i = 0; i < 5; i++) {
+    dice[i] = (rand() % 6) + 1;
+
+cout << "You rolled a " << dice[i] << " on dice " << (i + 1) << "." << endl;
+}
+
+
 return 0;
 
 
